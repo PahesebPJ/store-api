10ff90e68596337a40bdee90f88041f6e0fe1712
paheseb <pahesebw@gmail.com>
2022-06-12 22:30:23 -0500

    Created public folder and added public folder middleware

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ app.set('port', config.port);
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: false}));
+app.use(express.static(__dirname + '/public'));
 
 //Routes
 app.use(productsRoute);
